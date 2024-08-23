663d4ae578ea9460a54e0d62b0ddc2ac6ffc3dc2
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2024-08-23 23:01:25 +0200

    Allow verifying token

---
+++ premier_geek/api/runtime/security.py
@@ -0,0 +1,21 @@
+import cognitojwt
+
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi.security import HTTPAuthorizationCredentials
+from fastapi.security import HTTPBearer
+from runtime.settings import settings
+from starlette import status
+
+
+security = HTTPBearer()
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> bool:
+    id_token = credentials.credentials
+    try:
+        id_token = credentials.credentials
+        cognitojwt.decode(id_token, settings.aws_region, settings.user_pool_id, settings.user_pool_client_id)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials.") from e
+    return True
